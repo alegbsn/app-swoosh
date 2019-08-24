057db8f6a282f7ab4b1ff8efd59ec959430f0c55
Alexandre Gibson <alexandregibson@gmail.com>
2019-08-24 20:54:42 -0300

    Build model, add segues and fixe third screen.

--- app-swoosh/app-swoosh/Controller/WelcomeViewController.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Alexandre Gibson on 24/08/19.
+//  Copyright © 2019 GBSN. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
